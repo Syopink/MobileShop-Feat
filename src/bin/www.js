33cb0c72e5109dc82226aa3bba386542f26b72c9
Nguyen Gia Huy <huyng1.bhsoft@gmail.com>
2025-11-14 09:32:34 +0700

    update logic

--- src/bin/www.js
+++ src/bin/www.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://mobileshop-2ujv.onrender.com",
     methods: ["GET", "POST"],
   },
 });
